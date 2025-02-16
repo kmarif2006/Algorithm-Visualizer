b97b7a918b3f6b7234e7bb0c54e55d95d2dbe2d2
kmarif2006 <arifkmofficial@gmail.com>
2025-02-17 01:03:11 +0530

    Add build script and configure static file serving for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,6 +23,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
