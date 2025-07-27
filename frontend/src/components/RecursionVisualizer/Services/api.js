ea54966e224b9c94375cc4303cb7d662d6bcd940
arifkm2006 <arifkmofficial@gmail.com>
2025-07-28 00:27:46 +0530

    Final commit

--- frontend/src/components/RecursionVisualizer/Services/api.js
+++ frontend/src/components/RecursionVisualizer/Services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://algorithm-visualizer-backend-znhz.onrender.com';
 
 export const generateRecursionTree = async (functionName, inputValue) => {
   try {
